daf3364d94a26452588199f2bc31261b08ad209a
Monica Rodriguez <ic.monica.rodriguez@gmail.com>
2016-08-26 17:35:39 -0700

    Started Trees and Graphs problems, pending testing on routeBetweenNodes

---
+++ Cracking the Coding Interview/Trees and Graphs/routeBetweenNodes.js
@@ -0,0 +1,41 @@
+/*
+*   Given a directed graph, design an algorithm to find out wheter there is a route between two nodes
+*   Assumptions: Values not repeated, if values can be repeated we should add uniqueIDs to nodes in this case we will supp
+*/
+
+function Node(value){
+    this.value = value;
+    this.connections = [];
+}
+
+function areConnected(node1,node2){
+    var hash = {}, q1 = [], q2 = [];
+    hash[node1.value] = "expanded1";
+    hash[node2.value] = "expanded2";
+    q1.push(node1);
+    q2.push(node2);
+    while(q1.length > 0 && q2.length > 0){
+        var tmp1 = q1.shift(), tmp2 = q2.shift();
+        if(expandNode(tmp1,q1,hash,"expanded1"))
+            return true;
+        if(expandNode(tmp2,q2,hash,"expanded2"))
+            return true;
+    }
+    return false;
+
+}
+
+function expandNode(node,q,hash,mark){
+    for(var i = 0; i < node.connections.length; i++){
+        var value = node.connections[i].value;
+        if(hash.hasOwnProperty(value)){
+            if( hash[value] != mark ){
+                return true;
+            }
+        }else{
+            hash[value] = mark;
+            q.push(node.connections[i])
+        }
+    }
+    return false;
+}
